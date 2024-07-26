0cfa3ada4646e44ab6632254f06200272bd7825c
Hadar <hadarnd8@gmail.com>
2024-07-26 15:21:04 +0300

    Add files via upload

---
+++ loop.1+2+3.py
@@ -0,0 +1,24 @@
+# # loop-1)
+top: int = int(input("enter a number:"))
+
+for i in range(1,top + 1):
+     print(i , end=",")
+
+
+#loop-2)
+x: int = int(input("enter first number:"))
+y: int = int(input("enter second number:"))
+
+if x > y:
+     for j in range(y, x+1):
+         print(j, end=",")
+else:
+     for j in range(x, y+1):
+         print(j, end=",")
+
+#loop-3)
+n: int = int(input("enter a number:"))
+
+for h in range(1 , n+1 ,2):
+    print(h, end=",")
+
